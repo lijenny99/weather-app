e5ec4aaadc650b176b5cbee0fb327a834da94eff
Jenny Li <lijenny1999@gmail.com>
2018-12-24 18:03:11 -0500

    update

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h5 className="title-container__title">Weather Finder</h5>
+    <p className="title-container__subtitle">Find out temperature, conditions, and more...</p>
+  </div>
+)
+
+export default Titles;
